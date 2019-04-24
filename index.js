423bddf1c65fcc228f71ab2279d19658f8241436
mbw3641193 <305683054@qq.com>
2019-04-24 18:35:02 +0800

    添加了引导页，尚未配置路由

--- index.js
+++ index.js
@@ -8,15 +8,27 @@ import {createAppContainer} from 'react-navigation';
 import {Provider} from 'react-redux';
 import store from './src/store';
 
+import { ThemeProvider } from 'react-native-elements';
+
 const AppStackNavigatorContainer = createAppContainer(AppNavigator);
 
+const theme = {
+    Button: {
+        titleStyle: {
+            color: '#fff',
+        },
+    },
+};
 const ReduxApp = () => (
     // 配置 Provider 为根组件，同时传入 store 作为其属性
+    
     <Provider store={store}>
-      {/* redux 持久化存储 */}
-      {/* <PersistGate persistor={persistor}> */}
-        <AppStackNavigatorContainer />
-      {/* </PersistGate> */}
+        <ThemeProvider theme={theme}>
+            {/* redux 持久化存储 */}
+            {/* <PersistGate persistor={persistor}> */}
+                <AppStackNavigatorContainer />
+            {/* </PersistGate> */}
+        </ThemeProvider>
     </Provider>
 );
 
--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,23 +1,59 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View,SafeAreaView} from 'react-native';
+import { ThemeProvider, Button } from 'react-native-elements';
 
+import Swiper from 'react-native-swiper';
+
+const styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
 
 export default class Welcome extends Component {
 
     componentDidMount(){
         console.log(this.props);
-        setTimeout(()=>{
-            this.props.navigation.navigate('Main');
-        },2000)
+        // setTimeout(()=>{
+        //     this.props.navigation.navigate('Main');
+        // },2000)
     }
 
   render() {
     return (
-      <SafeAreaView>
-        <View>
-            <Text style={{textAlign:'center',marginTop:200,fontSize:30}}>Welcome</Text>
+        <Swiper style={styles.wrapper} loop={false}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
         </View>
-      </SafeAreaView>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
     );
   }
 }
